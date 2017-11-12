4300c394be2519ca0188062077e0e393c1540bec
Abhijeet Rastogi <me@abhijeetr.com>
2017-11-12 11:45:15 +0530

    Add actual code to push to ES

--- es.go
+++ es.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"context"
 	"log"
 	"sync"
+
+	elastic "gopkg.in/olivere/elastic.v5"
 )
 
 func esIndexer(c config, wg *sync.WaitGroup) {
+	ctx := context.Background()
+	client, err := elastic.NewClient()
+	if err != nil {
+		log.Fatalf("Error connecting to ES: %s", err)
+	}
+
 	for i := 0; i < len(c.Tracks); i++ {
-		log.Printf("ES Index: %s", <-results)
+		doc := <-results
+		_, err := client.Index().
+			Index("commute-time").
+			Type("doc").
+			BodyJson(doc).
+			Do(ctx)
+		if err != nil {
+			log.Printf("Error indexing document: %s", err)
+		}
+		log.Printf("Indexed to ES: doc=%s", doc)
 	}
 	wg.Done()
 }
