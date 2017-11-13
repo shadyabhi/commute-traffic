3187567fb18309c4be2ea5d8daafff817cfaa280
Abhijeet Rastogi <me@abhijeetr.com>
2017-11-13 07:28:15 +0530

    Create index with proper mapping if it doesn't exist

--- es.go
+++ es.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"sync"
 
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
+const mapping = `
+    doctype: {
+        'properties': {
+            'timestamp': { 'type': 'date', 'format': 'epoch_second', 'store': True },
+            'start_address': { 'type': 'string', 'store': True },
+            'end_address': { 'type': 'string', 'store': True },
+
+            'distance': { 'type': 'long', 'store': True },
+            'duration': { 'type': 'long', 'store': True },
+            'duration_in_traffic': { 'type': 'long', 'store': True },
+        }
+    }
+}`
+
 func esIndexer(c config, wg *sync.WaitGroup) {
 	ctx := context.Background()
 	client, err := elastic.NewClient()
 	if err != nil {
 		log.Fatalf("Error connecting to ES: %s", err)
+	}
+	err = createIndex(client, "commute-time", ctx)
+	if err != nil {
+		log.Printf("Error creating index in ES: %s", err)
 	}
 
 	for i := 0; i < len(c.Tracks); i++ {
@@ -28,4 +47,17 @@ func esIndexer(c config, wg *sync.WaitGroup) {
 		log.Printf("Indexed to ES: doc=%s", doc)
 	}
 	wg.Done()
+}
+
+func createIndex(client *elastic.Client, name string, ctx context.Context) error {
+	exists, err := client.IndexExists(name).Do(ctx)
+	if err != nil {
+		return fmt.Errorf("Error checking if index exists: %s", err)
+	}
+	if !exists {
+		if _, err := client.CreateIndex(name).BodyString(mapping).Do(ctx); err != nil {
+			return fmt.Errorf("Error creating index: %s", err)
+		}
+	}
+	return nil
 }
